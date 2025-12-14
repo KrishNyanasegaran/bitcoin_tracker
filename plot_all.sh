b5f0b4f581de3fb7d78baf95529f7d7875e1e7d3
KrishNyanasegaran <krishnasalim7089@gmail.com>
2025-12-14 23:40:28 +0800

    Update plot_all.sh added lines of codes

--- plot_all.sh
+++ plot_all.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+cd ~/bitcoin_tracker
+
+./plot.sh price
+./plot.sh low
+./plot.sh high
+./plot.sh price_points
+./plot.sh low_points
+./plot.sh high_points
+./plot.sh price_vs_low
+./plot.sh price_vs_high
+./plot.sh low_vs_high
+./plot.sh history
+
+echo "All 10 plots generated."
